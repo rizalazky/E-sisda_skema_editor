e36575e7e4386adf45ec0636f49552e2f2b0dbba
Reza Zulfan Azmi <zarezazul@gmail.com>
2021-02-28 16:11:31 +0700

    add nilai qdiberi

--- filebendung/getBendung.php
+++ filebendung/getBendung.php
@@ -1,15 +1,34 @@
 <?php
-    include './../conn.php';
-    $query=mysqli_query($conn,"select id,lokasi,a_tanam,q_diberikan FROM alokasi WHERE id in (SELECT MAX(alo.id) FROM alokasi alo GROUP BY alo.lokasi) AND di='$_GET[kode_bendung]' ORDER BY id DESC");
-  
-    $data=array();
-    if(!$query){
-      echo mysqli_error($conn);
-      die;
-    }else{
-        while($row=mysqli_fetch_array($query)){
-            array_push($data,$row);
-        }
+include 'conn.php';
+include '';
+$query = mysqli_query($conn,"select id,lokasi,a_tanam,q_diberikan,periode,bulan,tahun FROM alokasi WHERE id in (SELECT MAX(alo.id) FROM alokasi alo GROUP BY alo.lokasi) AND di='$_GET[kode_bendung]' ORDER BY id DESC");
+$q_debit = mysqli_query($conn, "SELECT debit from elevasi WHERE kode='$_GET[kode_bendung]' ORDER BY id_elevasi DESC");
+$period = $dt['periode'];
+$blan = $dt['bulan'];
+$thn = $dt['tahun'];
+
+$bulan = getBulan($blan);
+
+$jmlhari = jumlahhari($blan, $thn);
+$data = array(
+    "query" => array(),
+    "q_debit" => array(),
+    "periode"=>$jmlhari,
+);
+if (!$query) {
+    echo mysqli_error($conn);
+    die;
+} else {
+    while ($row = mysqli_fetch_array($query)) {
+        array_push($data["query"], $row);
     }
-    
-    echo json_encode($data);
+    while ($debit = mysqli_fetch_array($q_debit)) {
+        array_push($data["q_debit"], $debit);
+    }
+}
+// var_dump($data);
+// die;
+// $debit = mysqli_fetch_array($q_debit);
+// arrat_push($data, $debit);
+
+echo json_encode($data);
--- filebendung/index.php
+++ filebendung/index.php
@@ -7,6 +7,7 @@
   <title>Document</title>
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="../header.css">
+  <link rel="stylesheet" href="detail.css">
 </head>
 
 <body>
@@ -16,46 +17,35 @@
 
 
   <?php
-  include '../conn.php';
+  include 'conn.php';
+  // include 'conn.php';
+  include 'template/header.php';
   if (!empty($_GET['kode_bendung'])) { ?>
-    <div class="headerCSS">
-      <div class="header__logoJateng">
-        <img class="animate__animated animate__swing animate__infinite infinite" src="../assets/header/logo_jateng.png" alt="Logo" />
-        <div class="header__title">
-          <h1>APEMASI</h1>
-          <span> BPSDA BODRI KUTO</span>
-        </div>
-      </div>
 
-      <div class="header__logoPublikasi">
-        <img src="../assets/header/gunung.png" alt="" srcset="" />
-        <div class="header__title">
-          <span class="publikasi_distibusi">
-            <marquee scrollamount="15">
-              PUBLIKASI DISTRIBUSI AIR IRIGASI</marquee>
-          </span>
-        </div>
-      </div>
-
-      <div class="header__logoJatengGayeng">
-        <img class="animate__animated animate__flipInY" src="../assets//header//logoJatengGayeng.png" alt="" srcset="" />
-      </div>
-    </div>
-    <div class='container'>
+    
+    <?php
+    include './detail.php';
+    ?>
     <?php
     include($_GET['kode_bendung'] . ".php");
   } else { ?>
-      <ul style='display:flex;flex-direction:column;'>
-        <?php $query = mysqli_query($conn, "SELECT kode,bendung FROM bendung");
-        while ($r = mysqli_fetch_array($query)) { ?>
-          <li>
-            <a href="?kode_bendung=<?php echo $r['kode']; ?>"><?php echo $r['bendung']; ?></a>
-          </li>
-      <?php
-        }
-      }
-      ?>
-      </ul>
+      <div class='list-bendung-container'>
+        <?php $query = mysql_query("SELECT kode,bendung FROM bendung");
+        while ($r = mysql_fetch_array($query)) { 
+          if(file_exists ($r['kode'].'.php')){
+          ?>
+          <div class="list-bendung-item">
+            <a href="?kode_bendung=<?php echo $r['kode']; ?>" class='list-bendung-card'>
+              <img src="../assets/images/mejagong1.jpg" alt="gambarbendung" class='list-bendung-image'>
+              <div class='list-bendung-desc'>
+                <?php echo $r['bendung']; ?>
+              </div>
+            </a>
+        </div>
+      <?php }} ?>
+      </div>
+<?php } ?>
+
       <script>
         let queryString = window.location.search;
         let UrlParams = new URLSearchParams(queryString)
@@ -63,19 +53,19 @@
         const table = document.getElementsByClassName('table');
         const lokasi = document.getElementsByClassName('lokasi');
 
-        console.log(lokasi)
+        // console.log(lokasi)
         let data = []
 
         function setValueTable() {
-          console.log(data)
+          // console.log(data)
           for (let i = 0; i < table.length; i++) {
             // console.log(table[i].children[1])
             // console.log(lokasi[i].value)
             let area, qAlir;
-            for (let x = 0; x < data.length; x++) {
-              if (lokasi[i].value == data[x].lokasi) {
-                area = data[x].a_tanam;
-                qAlir = data[x].q_diberikan
+            for (let x = 0; x < data["query"].length; x++) {
+              if (lokasi[i].value == data["query"][x].lokasi) {
+                area = data["query"][x].a_tanam;
+                qAlir = data["query"][x].q_diberikan
               }
             }
             table[i].children[1].innerHTML = `
@@ -91,20 +81,30 @@
           }
         }
 
-        setInterval(() => {
-          fetchData()
-        }, 1000)
+      
+        fetchData()
 
-
+        let qdiberi=[]
+        let lok=[]
         function fetchData() {
-          console.log('test')
+          // console.log('test')
           fetch('http://localhost/E-sisda_skema_editor/filebendung/getBendung.php?kode_bendung=' + KodeBendung).
           then(res => {
             return res.json()
           }).then(result => {
             data = result
-            console.log('h: ', result)
+            // console.log(data["query"][0])
+
+            for (let z = 0; z < data["query"].length; z++) {
+              const dt = data["query"][z];
+              qdiberi.push(dt.q_diberikan)
+              lok.push(dt.lokasi)
+              
+            }
+            
             setValueTable()
+            setSum();
+            setDetail();
           }).catch(err => console.log(err))
         }
         const select = document.getElementsByTagName('select')
@@ -119,11 +119,81 @@
           text[index].setAttribute('contenteditable', false)
           text[index].style.textAlign = 'center'
         }
+
+        function setSum() {
+          for (let index = 0; index < text.length; index++) {
+            const element = text[index];
+            let nilaiid=element.dataset.nilaiid
+            let q_berikan=element.dataset.qberikan
+            if( nilaiid!=null || q_berikan!=null ){
+              sumDebit(text[index].id,nilaiid,q_berikan)
+              // document.getElementById(nilaiid).innerHTML =setvalue
+              // console.log('panggil func'+index)
+            }
+          }
+        }
+        
+        function sumDebit(id,nl,qberi) {
+          let docid=document.getElementById(id)
+          let element='';
+          let nilai=0;
+          let totaldebit=0
+          if (nl!=undefined && nl!="" && nl !=null && nl!='undefined' ) {
+            let arrayNilai = nl.split(",")
+            for (let index = 0; index < arrayNilai.length; index++) {
+              element=document.getElementById(arrayNilai[index])
+              nilai=Number(element.textContent)+nilai
+              
+            }
+          }
+
+          totaldebit=totaldebit+Number(nilai)
+
+          let array=qberi.split(",")
+          // console.log(array)
+          // let lokasibaru=
+          if(lok.length >0){
+            array.map(l=>{
+
+              for (let s = 0; s < lok.length; s++) {
+                // console.log(lok[s])
+                if(data["query"][s].lokasi==l){
+                  totaldebit=totaldebit+ Number(qdiberi[s])
+                  // console.log(qdiberi[s],'qdiberi')
+                 
+                  // return totaldebit
+                }
+                
+              }
+              // console.log(totaldebit, array)
+              
+            })        
+          }
+          // console.log(totaldebit, 'cek')
+          docid.innerHTML=totaldebit.toFixed(2)
+          // console.log(element)
+        }
+
+        
+        function setDetail(){
+          let debit = 0
+          let diperlukan = Number(data["q_debit"][0].debit)
+          for (let i = 0; i < qdiberi.length; i++) {
+            debit = debit + Number(qdiberi[i]); 
+          }
+          console.log(data["q_debit"])
+          let debitTersedia = document.getElementById('debittersedia')
+          let debitDiperlukan = document.getElementById('debitdiperlukan')
+          let faktorK = document.getElementById('faktorK')
+          debitDiperlukan.innerHTML = diperlukan.toFixed(2)
+          debitTersedia.innerHTML = debit.toFixed(2)
+          faktorK.innerHTML = debit/diperlukan > 1 ? '1.00' : (debit/diperlukan).toFixed(2)
+
+        }
       </script>
-    </div>
+
     <!-- <div class="footer">
       Copyright @2021 pemalicomal
     </div> -->
 </body>
-
 </html>
